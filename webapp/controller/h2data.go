dc3fca15573c8c39830896bd2485cce6486990c6
vlad <ovlad32@gmail.com>
2016-10-06 02:47:53 +0300

    progress on boltdb-1

---
+++ webapp/controller/h2data.go
@@ -0,0 +1,15 @@
+package controller
+
+import (
+	"../../metadata"
+	"net/http"
+	"encoding/json"
+)
+
+func GetDC(w http.ResponseWriter, r *http.Request){
+	result,err := metadata.H2.DatabaseConfigAll()
+	if err != nil {
+		panic(err)
+	}
+	json.NewEncoder(w).Encode(result)
+}
